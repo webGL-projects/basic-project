4c1c69bc2bc7ff066a31383b2e97d4ddce91922f
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-06-26 16:06:36 +0300

    Studied some types of build-in Three.JS controls

--- basic-scene/exercise/07-cameras/src/script.js
+++ basic-scene/exercise/07-cameras/src/script.js
@@ -112,8 +112,33 @@ const tick = () =>
     camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
     camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
     camera.position.y = cursor.y * 2;
-    
+
     camera.lookAt(mesh.position) // move the camera arounfd the object still can't see the back of the cube
+    
+    /* You use build-in ThreeJS controlsb to do the same thing
+    
+    1. fly control enable moving the camera like in a spcaeship
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/FlyControls
+
+    2. First Person Control like fly control but in fixed axis doesn't work on FPS games
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/FirstPersonControls
+
+    3. Pointer lock control: it uses the pointer lock JS API hard to use and almost only handles the pointer lock and camera rotation
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/PointerLockControls
+
+    4. Orbit Control similiar to the other controls but with more features (this will be used)
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/OrbitControls
+
+    5. Trackball control: like orbitControls without the vertical angle limit
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/TrackballControls
+
+    Transform control: has nothing to do with camera control
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/TransformControls
+
+    Darg control: it has nothing to do with camera but it is used to grag objects
+    https://threejs.org/docs/index.html?q=controls#examples/en/controls/DragControls
+    */
+    
     // Render
     renderer.render(scene, camera)
 
