91201240d5b48c32245f6cf8218c8b2de0aec9cc
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-07-13 12:11:12 +0300

    Finished Shadows

--- basic-scene/exercise/16-shadows/readme.md
+++ basic-scene/exercise/16-shadows/readme.md
@@ -10,7 +10,66 @@ Three.js has a built-in solution, it's not perfect but it's convenient.
 * when we do one render, Threejs will do a render for each light supporting shadows 
 * Those renders will simulate what the light sees as if it was a camera
 * During these lights renders, a MeshDepthMaterial replaces all meshes materials
+* The lights renders are stored as textures and we call those shadow maps
+* They are then used on every material supposed to recieve shadows and project on the geometry 
 
 MeshDepthMaterial: A material for drawing geometry by depth. Depth is based off of the camera near and far plane. White is nearest, black is farthest.
 
 https://threejs.org/docs/index.html#api/en/materials/MeshDepthMaterial
+
+we need to activate the shadow maps on the renderer
+then we need to go through each object and decide if it can cast a shadow with castShadow and if it can recieve shadow with receivedShadow
+
+as a reminder the lights that cast a shadow are 
+ 1. pointLight
+ 2. DirectionalLight
+ 3. SpotLight
+
+we need to activate the shadows on the light with castShadow
+
+## Shadow Map optimization
+it can be done by controlling:
+
+ 1. Render Size: we can access the shadow map in the shadow property of each light
+ By default the size is 512x512, wwe can improve it but we need to keep 0f power of 2 for the mipmapping
+
+ 2. Near and Far: it is more about precision 
+ Threejs is using cameras to do the shadow maps renders those cameras have the same properties like near and far
+
+ to help us debug, we can use a CameraHelper with the camera used for the shadow map in the directionalLight.shadow.camera
+
+ 3. Amplitude: we can see the amplitude is too large, because we are using a DirectionalLight, Threejs using and OrthographicCamera 
+
+ the smaller the values, the more precise the shadow will be  if it's too small the shadows will be cropped
+
+ 4. Blur: it can be controlled using the raduis property, this technique use the proximity of the camera with the object, it's a general and cheap blur
+
+ 5. Shadow Map Algorithm: there are different types of shadow maps
+  a. THREE.BasicShadowMap - Very performant but lousy quality
+  b. THREE.PCFShadowMap - Less performant but smoother edges (default)
+  c. THREE.PCFSoftShadowMap - Less Performant but even softer edges The radius doesn't work
+  d. THREE.VSMShadowMap - Less performant, more contraints, can have unexpected results 
+
+## SpotLight Shadow Map 
+Mixing Shadow doesn't look good and there is not much to do about it
+
+same techniques used in the directional light 
+
+Spot Light uses Perspective camera we must change the fov propertu to adopt the amplitude
+
+## PointLight Shadow Map
+the camera helper used is a perspectiveCamerafacing downward Three.js uses a perspectiveCamera but in all 6 directions and finishes downward
+
+same controls as before
+
+# Baking Shadows
+a good alternative to Three.js shadows are baked , we integrate shadows in textures that we apply on materials
+
+we need to deactivate the shadows (it may cause an error when using castShadow)
+
+the issue with it is that it is not dynamic 
+
+## Baking Shadows Alternative
+we can also use a more simpler baked shadow and move it so is stays under the sphere 
+
+create a plane slightly above the floor with an alphaMap using the simpleShadow
--- basic-scene/exercise/16-shadows/src/script.js
+++ basic-scene/exercise/16-shadows/src/script.js
@@ -5,6 +5,13 @@ import * as dat from 'lil-gui'
 THREE.ColorManagement.enabled = false
 
 /**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const bakedTexture = textureLoader.load('/textures/bakedShadow.jpg')
+const simpleShadow= textureLoader.load('/textures/simpleShadow.jpg')
+/**
  * Base
  */
 // Debug
@@ -20,18 +27,104 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+
+// Activate the shadows
+directionalLight.castShadow = true
+console.log(directionalLight.shadow)
+
+// in case of an error
+directionalLight.castShadow = false
+
+// Render Size Optimization 
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+
+// Near and Far
+
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = - 2
+directionalLight.shadow.camera.left = - 2
+
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+
+// Directional Camera Helper used to help us find the near and far
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+// to hide the helper
+directionalLightCameraHelper.visible = false
+
+// Blur
+directionalLight.shadow.radius = 10
+
 scene.add(directionalLight)
+
+// Spot Light
+const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI *0.3)
+
+spotLight.castShadow = true
+spotLight.position.set(0, 2, 2)
+
+// in case of an error
+spotLight.castShadow = false
+
+scene.add(spotLight, spotLight.target)
+
+// Map Size
+spotLight.shadow.mapSize.width = 2048
+spotLight.shadow.mapSize.height = 2048
+
+// Feild of Veiw = amplitude
+spotLight.shadow.camera.fov = 30
+
+
+// Near and Far
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
+
+// Spot Light Helper
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+scene.add(spotLightCameraHelper)
+
+spotLightCameraHelper.visible = false
+
+// Point Light
+const pointLight = new THREE.PointLight(0xffffff, 0.3)
+
+pointLight.castShadow = true
+pointLight.position.set(-1, 1, 0)
+
+// in case of an error
+pointLight.castShadow = true
+
+scene.add(pointLight)
+
+// Map Size
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+
+// Near and Far
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 5
+
+
+// Point Light Helper
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+scene.add(pointLightCameraHelper)
+
+pointLightCameraHelper.visible = false
 
 /**
  * Materials
@@ -49,14 +142,40 @@ const sphere = new THREE.Mesh(
     material
 )
 
+
+// the sphere cast a shadow 
+sphere.castShadow = true
+
+
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(5, 5),
+//     new THREE.MeshBasicMaterial({
+//         map: bakedTexture
+//     })
+//     )
+
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    material
-)
+        material
+    )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
-scene.add(sphere, plane)
+// The plane receives a shadow
+plane.receiveShadow = true
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x00000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphere, sphereShadow, plane)
 
 /**
  * Sizes
@@ -105,6 +224,17 @@ renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+// we need to activate shadow maps in the renderer
+renderer.shadowMap.enabled = true
+
+// Shadow Map Algorithm - update the shadow map
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+// Deactivate the shadows to bake them in
+renderer.shadowMap.enabled = false
+
+
+
 /**
  * Animate
  */
@@ -113,6 +243,17 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update the sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3 ))
+
+        // Update the sphere Shadow
+        sphereShadow.position.x = Math.cos(elapsedTime) * 1.5
+        sphereShadow.position.z = Math.sin(elapsedTime) * 1.5
+        sphereShadow.material.opacity = (1 - Math.abs(sphere.position.y)) * 0.3
+    
 
     // Update controls
     controls.update()
