7f4d5d8a94fb2b0f19f9a05d857e9a71b4dbd3c6
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-07-18 12:04:15 +0300

    Finished Galaxy Generator

--- basic-scene/exercise/19-galaxy-generator/readme.md
+++ basic-scene/exercise/19-galaxy-generator/readme.md
@@ -6,3 +6,11 @@ when using debug UI the previous galaxies are not destroyed
 to solve it we need to move the geometry, material and points variables outside the generateGalaxy
 
 before assigning those variables, we can test if they already exist and use the dispose() method to destroy the geometry and the material properly then remove the points from the scene with remove()
+
+we need to position the vertices in a straight line starting from the center and going as far as the radius 
+
+## Color
+
+https://threejs.org/docs/index.html#api/en/math/Color
+
+we can mix colors by cloning the colorInside and use the lerp() method to mix with the colorOutside
--- basic-scene/exercise/19-galaxy-generator/src/script.js
+++ basic-scene/exercise/19-galaxy-generator/src/script.js
@@ -22,7 +22,14 @@ const scene = new THREE.Scene()
 
 const parameters = {
     count: 100000,
-    size: 0.01
+    size: 0.01,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.2,
+    randomnessPower: 3,
+    insideColor: '#ff6030',
+    outsideColor: '#1b3984'
 }
 
 let geometry = null
@@ -42,15 +49,41 @@ if(points !== null){
 // Geometry
   geometry = new THREE.BufferGeometry()
   const positions = new Float32Array(parameters.count * 3)
- 
+
+  const colors = new Float32Array(parameters.count * 3)
+  const colorInside = new THREE.Color(parameters.insideColor)
+  const colorOutside = new THREE.Color(parameters.outsideColor) 
+
+
  // Particle Positions
  for(let i=0; i< parameters.count; i++){
     const i3 = i * 3
-    positions[i3] =  (Math.random() - 0.5) * 3
-    positions[i3 + 1] =  (Math.random() - 0.5) * 3
-    positions[i3 + 2] =  (Math.random() - 0.5) * 3
+
+    //Position
+    const raduis = Math.random() * parameters.radius
+    const barnchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+    const spinAngle = raduis * parameters.spin
+
+    const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1) * parameters.randomness * raduis
+    const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1) * parameters.randomness * raduis
+    const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1: -1) * parameters.randomness * raduis
+   
+    positions[i3    ] =  Math.cos(barnchAngle + spinAngle) * raduis + randomX
+    positions[i3 + 1] =  randomY 
+    positions[i3 + 2] =  Math.sin(barnchAngle + spinAngle) * raduis + randomZ
+
+    // Color
+    const mixedColor = colorInside.clone()
+    mixedColor.lerp(colorOutside, raduis / parameters.radius)
+    
+    colors[i3    ] = mixedColor.r
+    colors[i3 + 1] = mixedColor.g
+    colors[i3 + 2] = mixedColor.b
+
+
  }
  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+ geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
 
  // Material
@@ -58,7 +91,8 @@ if(points !== null){
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
-    blending: THREE.AdditiveBlending
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
  })
 
  // Points
@@ -71,6 +105,13 @@ generateGalaxy()
 // Debug UI
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step( 1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 
 /**
