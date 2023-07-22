da5c8d1bafe692a52ea0af8e6d699e669c218184
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-07-22 19:03:18 +0300

    Finished Scroll Based Animations

--- basic-scene/exercise/20-scroll-based-animation/readme.md
+++ basic-scene/exercise/20-scroll-based-animation/readme.md
@@ -1,3 +1,82 @@
-# Scrool Based Animation 
+# Scroll Based Animation 
 
+It is used in classic websites, it can be in the background to add beauty, but we need it to intergrate properly with the HTML content
+
+## HTML Scroll 
+YOU NEED TO REMOVE ANY overflow: hidden in your css
+
+there this issue called elastic scroll (when you scroll too far you get elastic animation, when the page goes beyond the limit) we need to solve it by:
+
+ 1. set the background-color of the wpage to the same color as the clearColor
+
+ 2. use clearColor transparent and only set the background-color on the page 
+ we need to set the alpha property to true 
+ add a background color 
+
+ ## Mesh Toon Material
+ is a light based material
+
+ https://threejs.org/docs/index.html?q=toon#api/en/materials/MeshToonMaterial
+
+ we need to add griedint texture because it has only two color one in light and one in shadow
+ the gradient is a very small images composed of 3 pixels, instead of choosing one of those 3 pixels according to the light, WebGL will try to merge them to solve it we need to use a mapFilter
+
+ in Three.js the feild of view is vertical, if you put one object on the top, one on the bottom and then resize the window, objects will stay at the top and at the bottom, this will help us with positioning 
+
+ ## Permanent Rotation
+ it is added to the objects!
+
+ we will add them to sectionMeshes array 
+
+ ## Camera
+ we need to retrive the scroll value! and it must be updated whenever the user scrolls!!
+
+ we use the scrollY to update the camera position in the tick function, but the camera updates so fast so sensitive and going in the wrong direction 
+
+ the scrollY is +ve when scrolling down but we need the camera to go down the y axis
+
+ the scrollY contains tthe amount of pixels that have been scrolled, if we scroll 100 pixels, the camera will go down of 100 units in the scene 
+
+ each section has the same size as the viewport, This means that when we scroll the istance of one viewport height, the camera should reach the next object
+
+to do that we need to devide the scrollY by the height of the viewport which is sizes.heigth 
+
+now the camera is going down by 1 unit, but the objectDistance is 4 units so we need to mltiply it by objectDistance
+
+## Position Object Horizontally 
+
+## Parallax
+it is the action of seeing one object through different observation points 
+This is done naturally by our eyes and it's how we feel the depth of things
+so we will apply a parallax by making the camera move horizantly and vertically according to mouse movements
+
+to acheive that we need to retrive the cursor position, by creating a cursor object with x and y properties, we can use those values to move the camera but we need to modify them 
+
+we tnned to normalize the values because the users will have a different
+
+it is better to have the values go from -0.5 to 0.5 tather than 0 to 1
+
+## Easing 
+the parallax animation fells a bit too mechanic and it's not realistic 
+we are going to add some easing also called 'smoothing' and we are going to use a well-known formula
+
+on each frame instead of moving the camera streight to the target, we are going to move it (let's say) a 10th closed to the distination
+Then on the next frame , another 10th closer etc. 
+
+if you test this experience in high frequancy screen, the tick function will be called more often and the camera will move faster toward the target , this not a big issue, but it's not accurate and it's preferable to havethe same result across devices as much as possible
+
+to solve it we need to use the time spent between each frame by caculatin the dalta, dalta will be very small (0..16s for 60FPS screen) 
+
+
+## Partcles
+we use it to make the experience more immersive
+
+BufferGeometry is used be cause we want to place the partcles ourselves
+
+## Triggered Rotations
+we need to know when to trigger it,
+
+First we need to know when we reach a section, there are plenty of ways for doing that and we cou;d even use a library, but in our case we can usr the scrollY value
+
+once a new section is identified we will animate to meshes, using GSAP
 
--- basic-scene/exercise/20-scroll-based-animation/src/script.js
+++ basic-scene/exercise/20-scroll-based-animation/src/script.js
@@ -1,93 +1,247 @@
-import * as THREE from 'three'
-import * as dat from 'lil-gui'
+import * as THREE from "three";
+import * as dat from "lil-gui";
+import gsap from 'gsap'
 
-THREE.ColorManagement.enabled = false
+THREE.ColorManagement.enabled = false;
 
 /**
  * Debug
  */
-const gui = new dat.GUI()
+const gui = new dat.GUI();
+
+/**
+ * Twxture Loader
+ */
+
+const textureLoader = new THREE.TextureLoader();
+
+const gradientTexture = textureLoader.load("/textures/gradients/5.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
 
 const parameters = {
-    materialColor: '#ffeded'
-}
+  materialColor: "#ffeded",
+};
 
-gui
-    .addColor(parameters, 'materialColor')
+gui.addColor(parameters, "materialColor").onChange(() => {
+  material.color.set(parameters.materialColor),
+  particleMaterial.color.set(parameters.materialColor)
+});
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// )
+// scene.add(cube)
+
+/**
+ * Objects
+ */
+
+// Material
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
+
+// Meshes
+const ObjectDistance = 4; // it will get handy!
+
+const torusMesh = new THREE.Mesh(
+  new THREE.TorusGeometry(1, 0.4, 16, 60),
+  material
+);
+
+const coneMesh = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+
+const torusKnotMesh = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+);
+
+// torusMesh.position.y = - ObjectDistance * 0
+coneMesh.position.y = -ObjectDistance * 1;
+torusKnotMesh.position.y = -ObjectDistance * 2;
+
+// object Positions
+torusMesh.position.x = 2
+coneMesh.position.x = -2
+torusKnotMesh.position.x = 2
+
+scene.add(torusMesh, coneMesh, torusKnotMesh);
+
+const sectionMeshes = [torusMesh, coneMesh, torusKnotMesh];
+
+// Particles
+// Geometry 
+const particlesConut = 200 
+const positions = new Float32Array(particlesConut * 3)
+ 
+for (let i = 0; i< particlesConut; i++){
+    positions[i*3] = (Math.random() - 0.5) * 10
+    positions[i*3 + 1] = ObjectDistance * 0.5 -  Math.random() * ObjectDistance * sectionMeshes.length
+    positions[i*3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+// Material
+const particleMaterial = new THREE.PointsMaterial({
+   color: parameters.materialColor,
+   sizeAttenuation: true,
+   size: 0.03 
+})
+ 
+// Points
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
+scene.add(particles)
+
+// lights
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
 })
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
+
+// Camera Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+cameraGroup.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+  alpha: true, // to help solve the elastic scroll
+});
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+let currentSection = 0; 
+window.addEventListener("scroll", () => {
+
+  scrollY = window.scrollY;
+//   console.log(scrollY);
+const newSection = Math.round(scrollY / sizes.height) // this works because each section is exactly 1 height of the viewport
+
+    if(newSection != currentSection){
+        currentSection = newSection
+        // console.log('Changed', currentSection)
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation, 
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3',
+                z: '+=1.5'
+            }
+        )
+    } // This doesn't work we need to change some code in the tick function 
+// console.log(newSection)
+});
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+let previouseTime = 0 // for Easing
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previouseTime
+  previouseTime = elapsedTime
 
-    // Render
-    renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Animate Camera
+    camera.position.y = - scrollY / sizes.height * ObjectDistance
 
-tick()
+    const parallaX = cursor.x * 0.5
+    const parallaY = - cursor.y * 0.5
+
+    // Easing Formula
+    cameraGroup.position.x  += (parallaX - cameraGroup.position.x) * deltaTime * 5
+    cameraGroup.position.y += (parallaY - cameraGroup.position.y) * deltaTime * 5 // this replaces the first one to fix it we will put the camera in a group
+
+  // Animate Meshes
+  for (const mesh of sectionMeshes) {
+    // mesh.rotation.x = elapsedTime * 0.1;
+    // mesh.rotation.y = elapsedTime * 0.12;
+
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
+  }
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
