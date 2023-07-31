3abf0c75cfd8e9d0f70555c0b18255b5ef8dbca5
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-07-31 09:34:47 +0300

    Added the 3DText to the scene

--- basic-scene/exercise/48-3d-text-with-r3f/README.md
+++ basic-scene/exercise/48-3d-text-with-r3f/README.md
@@ -1,1 +1,27 @@
 ## 3D Text in R3F 
+There is a text3D helper 
+
+https://github.com/pmndrs/drei
+
+ to use it we need to provide a typeface font 
+
+ we can create our own Typeface with facetypy website
+
+ http://gero3.github.io/facetype.js/
+
+By default a meshBasicMaterial is applied by default
+
+Centering: there is a center helper provided from drei 
+
+all the attributes used to create the TextGeometry can be used  
+
+Matcaps: drei has a useMadCapTexture helper that will load matCap automatically from the repo 
+
+https://github.com/emmelleppi/matcaps
+https://github.com/nidorx/matcaps/blob/master/PAGE-17.md#7b5254_e9dcc7_b19986_c8ac91
+
+it is not recomended to use this technique in production because we are dependent on the server that provides the textures
+
+Text3D triggers a re-render on out component, its OK. it returns an array the first value is what we care about, so we destruct the const
+
+the useMatCapHelper accept 2 attributes, the ID of the Mat Cap and the sceond width (64-1024)
--- basic-scene/exercise/48-3d-text-with-r3f/src/Experience.jsx
+++ basic-scene/exercise/48-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,34 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls, Text3D, Center, useMatcapTexture } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
 
-        <Perf position="top-left" />
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+    console.log(matcapTexture)
+ 
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+      <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+      <Center>
+        <Text3D 
+        font="./fonts/helvetiker_regular.typeface.json"
+        size={ 0.75 }
+        height={ 0.2 }
+        curveSegments={ 12 }
+        bevelEnabled
+        bavelThickness={ 0.02 }
+        bavelSize={ 0.02 }
+        bavelOffset={ 0 }
+        bavelSegments={ 5 }
+        >
+          Hello R3F
+          <meshMatcapMaterial matcap={matcapTexture} />
+          
+        </Text3D>
+      </Center>
     </>
+  );
 }
