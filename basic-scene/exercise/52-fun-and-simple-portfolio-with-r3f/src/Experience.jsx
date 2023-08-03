570c8b0e50a9468ab181a1e3365d01d47d4c93f9
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2023-08-03 11:36:36 +0300

    Added a background color

--- basic-scene/exercise/52-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ basic-scene/exercise/52-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -4,6 +4,8 @@ export default function Experience()
 {
     return <>
 
+        <color args={ ['#241a1a']} attach='background' /> 
+
         <OrbitControls makeDefault />
         
         <mesh>
